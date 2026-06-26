be812d840d5fa1cd5a33cb971d5c342736502ef6
MonjurulArif <monjurulopu.com>
2026-06-26 17:21:18 +0600

    React Persist to keep the logged in and cart items after refresh

--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
 import cartReducer from "@/store/slices/cartSlice";
 import authReducer from "@/store/slices/authSlice";
 
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
